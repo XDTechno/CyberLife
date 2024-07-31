280766f43918b3c5a6c3dc5199dcad0742428139
胡龄 <2652328248@qq.com>
2024-07-31 08:44:45 +0800

    Refactor:world. added some function and now just use world[posx,posy] instead of world.map[posx][posy]

--- core/world/world.py
+++ core/world/world.py
@@ -12,16 +12,22 @@ actually has pseudo-depth
 
 class World:
     width:2
-    height:2
-    map:list[list[Tile]]
-    unit:list[Unit]
-
     @property
     def width(self):
-        return len(self.map)
+        return len(self.map[0])
+    height:2
     @property
     def height(self):
-        return len(self.map[0])
+        return len(self.map)
+    
+    map:list[list[Tile]]
+    #y lines ,each line contains x elements
+    # [
+    #   [x1y1,x2y1,x3y1],
+    #   [x1y2,x2y2,x3y2],
+    #   [x1y3,x2y3,x3y3]
+    # ]
+    unit:list[Unit]
 
     def __init__(self) -> None:
         self.map = [[Tile(), Tile()], [Tile(), Tile()]]
@@ -30,7 +36,6 @@ class World:
     def new_size(width,height):
         res=World()
         res.map=[[Tile() for _ in range(width)] for _ in range(height)]
-
         return res
 
     def add_unit(self, u: Unit):
@@ -39,25 +44,48 @@ class World:
         if (not hasattr(self, "unit")): self.unit: list[Unit] = []
         self.unit.append(u)
         try:
-            self.map[u.pos_x][u.pos_y].append(u)
+            self[u.pos_x,u.pos_y].append(u)
         except:
             pass
     def __getitem__(self, key):
         if not isinstance(key, tuple) or len(key) != 2:
             raise IndexError("Index must be a tuple of length 2")
-
         posx, posy = key
         if posx < 0 or posx >= self.width or posy < 0 or posy >= self.height:
             return None
+        else: return self.map[posy][posx]
     def __setitem__(self, key, value):
         if not isinstance(key, tuple) or len(key) != 2:
             raise IndexError("Index must be a tuple of length 2")
 
         posx, posy = key
         if posx < 0 or posx >= self.width or posy < 0 or posy >= self.height:
-            raise IndexError("Index out of range")
+            raise IndexError("Setting items out of range")
 
         self.map[posy][posx] = value
 
-        return self.map[posy][posx]
+        return self
+    def closest_map(self,_from:tuple,item,**args):
+        px,py=_from
+        collectee=[]
+        filter=item if callable(item) else lambda i:item in i
+        distance=lambda dx,dy:abs(dx**2+dy**2) if "distance" not in args else args['distance']
+        for ir, row in enumerate(self.map):
+            for ic, cell in enumerate(row):
+                if filter(cell) :
+                    collectee.append((ic, ir, distance(ir - px,ic - py)))
+        if len(collectee)>0:
+            return min(collectee,lambda n:n[2])
+        return None
+    def closest_unit(self,_from:tuple,item,**args):
+        px,py=_from
+        collectee=[]
+        filter=item if callable(item) else lambda i:item in i
+        distance=lambda dx,dy:abs(dx**2+dy**2) if "distance" not in args else args['distance']
+        for idx,u in enumerate(self.unit):
+            if(filter (u)):
+                collectee.append((idx,distance(u.pos_x-px,u.pos_y-py)))
+        if len(collectee)>0:
+            return min(collectee,lambda n:n[2])
+        return None
     #other status
